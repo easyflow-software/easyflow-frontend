a7389c7c92ad01564ec6f8fa974d3d15f284e967
Nico Benninger <nico.benninger43@gmail.com>
2024-08-21 10:36:12 +0200

    fix: matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,7 +32,22 @@ const middleware = async (req: NextRequest): Promise<NextResponse> => {
 
 // applies this middleware only to files in the app directory
 export const config = {
-  matcher: '/((?!api|static|.*\\..*|_next).*)',
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
 };
 
 export default middleware;
