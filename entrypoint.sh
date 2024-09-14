5fa5684bf6dd51394dc0ea8424a60b652407c059
Nico Benninger <nico.benninger43@gmail.com>
2024-09-14 13:06:25 +0200

    entrypoint broken

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 #Exit when error happens
 set -e
 
-printenv $CLOUDFLARE_ORIGIN_CERTIFICATE > /etc/ssl/easyflow.pem
-printenv $CLOUDFLARE_ORIGIN_CA_KEY > /etc/ssl/easyflow.key
+echo $CLOUDFLARE_ORIGIN_CERTIFICATE > /etc/ssl/easyflow.pem
+echo $CLOUDFLARE_ORIGIN_CA_KEY > /etc/ssl/easyflow.key
 
 #Start application
 NODE_ENV=production bun --bun run start &
