94bf3efd9b5604d8a6ade61671f32a0686c441fe
Nico Benninger <nico.benninger43@gmail.com>
2024-09-17 15:26:45 +0200

    fix(cors): Access-Control-Allow-Origin header

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,31 @@
 /** @type {import('next').NextConfig} */
 
+import AppConfiguration from './src/config/app.config';
+
 const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
     img-src 'self' https://easyflow-profile-pictures.d8ba15d176a1147e8cb7be257f6b18fb.eu.r2.cloudflarestorage.com;
-    connect-src 'self' ${process.env.NODE_ENV === 'production' ? 'https://backend.easyflow.chat' : 'http://localhost:4000'};
+    connect-src 'self' ${AppConfiguration.get('REMOTE_URL')};
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
-`
+`;
 
 const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
     key: 'Content-Security-Policy',
     value: cspHeader.replace(/\n/g, ''),
   },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS#access-control-allow-origin
   {
-    key: "Access-Control-Allow-Origin",
-    value: process.env.NODE_ENV === 'production' ? 'https://backend.easyflow.chat' : 'http://localhost:4000',
+    key: 'Access-Control-Allow-Origin',
+    value: AppConfiguration.get('REMOTE_URL'),
   },
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
   {
@@ -72,7 +76,7 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: process.env.NODE_ENV === 'production' ? ['https://easyflow.chat'] : ['*'],
+      allowedOrigins: [AppConfiguration.get("BASE_URL")],
     },
   },
   eslint: {
