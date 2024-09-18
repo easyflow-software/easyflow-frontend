d15a304e690d77b95de049712068dd0fcb10dd40
Nico Benninger <nico.benninger43@gmail.com>
2024-09-18 21:30:54 +0200

    NEXT_PUBLIC_STAGE maybe

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -2,21 +2,21 @@ import { VariablesType } from '../types/variables.type';
 
 let variables: VariablesType;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NEXT_PUBLIC_STAGE === 'production') {
   variables = {
     BASE_URL: 'https://easyflow.chat/',
     REMOTE_URL: 'https://backend.easyflow.chat/',
   };
 }
 
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NEXT_PUBLIC_STAGE === 'test') {
   variables = {
     BASE_URL: 'https://dev.easyflow.chat/',
     REMOTE_URL: 'https://dev.backend.easyflow.chat/',
   };
 }
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NEXT_PUBLIC_STAGE === 'development') {
   variables = {
     BASE_URL: 'http://localhost:3000/',
     REMOTE_URL: 'http://localhost:4000/',
