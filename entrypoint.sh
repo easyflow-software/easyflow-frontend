747c048271a8400bc3946d3da9221625304a6862
Nico Benninger <nico.benninger43@gmail.com>
2024-09-18 20:20:52 +0200

    maybe now

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-#Exit when error happens
+# Exit when error happens
 set -e
 
 # Secrets
 echo "$CLOUDFLARE_ORIGIN_CERTIFICATE" > /etc/ssl/easyflow.pem
 echo "$CLOUDFLARE_ORIGIN_CA_KEY" > /etc/ssl/easyflow.key
 
-#Start application
+# For debugging
+echo $NODE_ENV
+
+# Start application
 npx next start &
 
-#Start nginx in the forground
+# Start nginx in the forground
 nginx -g 'daemon off;'
--- src/config/variables.ts
+++ src/config/variables.ts
@@ -4,22 +4,22 @@ let variables: VariablesType;
 
 if (process.env.NODE_ENV === 'development') {
   variables = {
-    BASE_URL: 'http://localhost:3000',
-    REMOTE_URL: 'http://localhost:3001',
+    BASE_URL: 'http://localhost:3000/',
+    REMOTE_URL: 'http://localhost:4000/',
   };
 }
 
 if (process.env.NODE_ENV === 'test') {
   variables = {
-    BASE_URL: 'https://dev.easyflow.chat',
-    REMOTE_URL: 'https://dev.backend.easyflow.chat',
+    BASE_URL: 'https://dev.easyflow.chat/',
+    REMOTE_URL: 'https://dev.backend.easyflow.chat/',
   };
 }
 
 if (process.env.NODE_ENV === 'production') {
   variables = {
-    BASE_URL: 'https://easyflow.chat',
-    REMOTE_URL: 'https://backend.easyflow.chat',
+    BASE_URL: 'https://easyflow.chat/',
+    REMOTE_URL: 'https://backend.easyflow.chat/',
   };
 }
 
