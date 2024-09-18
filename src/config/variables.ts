ed194cdce167245294854b407e7047ec84ee2e16
Nico Benninger <nico.benninger43@gmail.com>
2024-09-18 21:05:14 +0200

    test again

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,17 +1,21 @@
 import { VariablesType } from '../types/variables.type';
 
 let variables: VariablesType;
-if (process.env.NODE_ENV === 'production' || window.location.href === 'https://easyflow.chat/') {
+if (window.location.href === 'https://easyflow.chat/' || process.env.NODE_ENV === 'production') {
   variables = {
     BASE_URL: 'https://easyflow.chat/',
     REMOTE_URL: 'https://backend.easyflow.chat/',
   };
-} else if (process.env.NODE_ENV === 'test' || window.location.href === 'https://dev.easyflow.chat/') {
+}
+
+if (window.location.href === 'https://dev.easyflow.chat/' || process.env.NODE_ENV === 'test') {
   variables = {
     BASE_URL: 'https://dev.easyflow.chat/',
     REMOTE_URL: 'https://dev.backend.easyflow.chat/',
   };
-} else {
+}
+
+if (window.location.href === 'http://localhost:3000/' || process.env.NODE_ENV === 'development') {
   variables = {
     BASE_URL: 'http://localhost:3000/',
     REMOTE_URL: 'http://localhost:4000/',
